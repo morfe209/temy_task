c5ab93d9b921f7ca097f89d695c6c922da677f04
Volodymyr Dziubanskyi <volodumur.Dzyubanskyy@gmail.com>
2018-12-04 09:27:14 +0200

    Create new front-end env for React

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Users from "./components/users";
+import UserForm from "./components/userForm";
+import axios from "axios";
+
+class App extends Component {
+  state = {
+    users: [],
+    countries: [],
+    states: [],
+    cities: [],
+    currentCountry: null,
+    currentState: null,
+    currentCity: null
+  };
+
+  async componentDidMount() {
+    const { data: users } = await axios.get("http://localhost:3001/users");
+    const { data: countries } = await axios.get(
+      "http://localhost:3001/countries"
+    );
+    const { data: states } = await axios.get("http://localhost:3001/states");
+    const { data: cities } = await axios.get("http://localhost:3001/cities");
+    this.setState({ users, countries, states, cities });
+  }
+  render() {
+    const { users, countries, cities, states } = this.state;
+    return (
+      <div className="container">
+        <Users
+          users={users}
+          countries={countries}
+          cities={cities}
+          states={states}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
